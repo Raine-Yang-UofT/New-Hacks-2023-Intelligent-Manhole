90914d1863a8ac761b1bfd403c813e79915b95ed
jerrywcy <jerrymonkeynj@163.com>
2023-11-05 08:51:40 -0500

    fix: change report data format

--- server/src/api/report.ts
+++ server/src/api/report.ts
@@ -1,10 +1,25 @@
 import { RequestHandler } from "express";
 import { connection } from "./index";
 
+const G = 9.80665;
 // /report/:manhole_id
 const report: RequestHandler = async function (req, res) {
     const { body, params } = req;
     const manhole_id = params.manhole_id;
+    const data = body.map((sensor_data: number[]) => {
+        return {
+            acceleration: {
+                x: sensor_data[0] * G,
+                y: sensor_data[1] * G,
+                z: sensor_data[2] * G,
+            },
+            gyro: {
+                x: (sensor_data[3] / 180.0) * Math.PI,
+                y: (sensor_data[4] / 180.0) * Math.PI,
+                z: (sensor_data[5] / 180.0) * Math.PI,
+            },
+        };
+    });
     let sqlQuery = `
     INSERT INTO Events ( 
         manhole_id, 
@@ -12,7 +27,7 @@ const report: RequestHandler = async function (req, res) {
     )
     VALUES (
         "${manhole_id}", 
-        '${JSON.stringify(body)}'
+        '${JSON.stringify(data)}'
     );
     `;
     connection.query(sqlQuery, (err, _) => {
